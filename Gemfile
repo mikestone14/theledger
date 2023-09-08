fdc932daea5d0d54ff94d755ae118b038fa21bf6
Mike Stone <mikestone14@gmail.com>
2023-09-07 20:34:57 -0400

    Upgrade to Rails 7

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'rails', '6.1.7.6'
+gem 'rails', '7.0.7.2'
 gem 'bcrypt', '~> 3.1.7'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 # Use postgresql as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_233923) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_09_08_003120) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +19,8 @@ ActiveRecord::Schema.define(version: 2020_09_14_233923) do
     t.bigint "winner_id", null: false
     t.bigint "loser_id", null: false
     t.integer "price_in_cents", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "status", default: 0, null: false
     t.index ["loser_id"], name: "index_games_on_loser_id"
     t.index ["season_id"], name: "index_games_on_season_id"
@@ -31,8 +30,8 @@ ActiveRecord::Schema.define(version: 2020_09_14_233923) do
   create_table "leaderboards", force: :cascade do |t|
     t.bigint "season_id"
     t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["season_id"], name: "index_leaderboards_on_season_id"
   end
 
@@ -42,8 +41,8 @@ ActiveRecord::Schema.define(version: 2020_09_14_233923) do
     t.integer "win_count", default: 0, null: false
     t.integer "loss_count", default: 0, null: false
     t.integer "net_in_cents", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.decimal "score", precision: 12, scale: 2, default: "0.0", null: false
     t.index ["leaderboard_id"], name: "index_records_on_leaderboard_id"
     t.index ["user_id"], name: "index_records_on_user_id"
@@ -51,8 +50,8 @@ ActiveRecord::Schema.define(version: 2020_09_14_233923) do
 
   create_table "seasons", force: :cascade do |t|
     t.integer "status", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "name"
     t.index ["status"], name: "index_seasons_on_status"
   end
@@ -61,8 +60,8 @@ ActiveRecord::Schema.define(version: 2020_09_14_233923) do
     t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.boolean "admin", default: false, null: false
     t.integer "status", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
